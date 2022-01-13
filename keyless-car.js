d571118d6cc83e97880cf099a0bfac548765b03e
TosinnOkewole <tosinokewole16@gmail.com>
2022-01-13 09:37:43 +0100

    first commit

---
+++ keyless-car.js
@@ -0,0 +1,5 @@
+var age = prompt("please enter your age")
+var result;
+if (age < 18) { alert("Sorry, you are too young to drive this car. Powering off"); }
+else if (age == 18) { alert("Congratulations on your first year of driving. Enjoy the ride!"); }
+else if (age > 18) { alert("Powering On. Enjoy the ride!"); }
